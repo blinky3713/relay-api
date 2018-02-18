d8a6ee12b2942668535606f7a33290483a11e13a
blinky3713 <blinky@foam.space>
2018-02-18 11:28:14 -0700

    fix tables

--- src/Relay/DB.hs
+++ src/Relay/DB.hs
@@ -5,8 +5,11 @@ module Relay.DB where
 import Relay.Types
 import qualified Database.Selda.Generic    as SG
 
-orders :: String -> SG.GenTable ExchangeOrder
-orders s = SG.genTable (s <> _"order") []
+radarOrders :: SG.GenTable ExchangeOrder
+radarOrders = SG.genTable ("radar_order") []
+
+dexOrders :: SG.GenTable ExchangeOrder
+dexOrders = SG.genTable ("dex_order") []
 
 tokens :: SG.GenTable Token
 tokens = SG.genTable "token" []
